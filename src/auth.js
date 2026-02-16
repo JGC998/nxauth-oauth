2ca39369b229362ee6bea5e72976ef3dccac39f9
JGC998 <juangarciacardenas99@gmail.com>
2026-02-16 19:27:21 +0100

    Arreglo en el .env

--- src/auth.js
+++ src/auth.js
@@ -19,6 +19,7 @@ export const {
     adapter: PrismaAdapter(prisma),
     session: { strategy: "jwt" },
     trustHost: true,
+    allowDangerousEmailAccountLinking: true,
     providers: [
         Google,
         GitHub,
